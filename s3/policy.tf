30420cf34ea7233360d1f9b70c891ad1032d0a39
João Dalla Rosa <joao.custodiodllrosa@gmail.com>
2026-02-19 18:45:27 -0600

    fix policy format

--- s3/policy.tf
+++ s3/policy.tf
@@ -1,18 +1,18 @@
 data "aws_iam_policy_document" "s3_policy" {
   statement {
     sid = "AllowCloudFrontRead"
-
+    effect = "Allow"
     principals {
       type        = "Service"
       identifiers = ["cloudfront.amazonaws.com"]
     }
 
     actions   = ["s3:GetObject"]
-    resources = ["arn:aws:s3::${local.bucket_name}/*"]
+    resources = ["arn:aws:s3:::${local.bucket_name}/*"]
 
     condition {
       test     = "StringEquals"
-      variable = "AWS:SourceArn"
+      variable = "aws:SourceArn"
       values = [
         "arn:aws:cloudfront::${local.account_id}:distribution/${local.cloudfront_distribution_id}"
       ]
